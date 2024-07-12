b4e78617c95bf7571d0c7b4dc5ad0699ef025976
x <x@y.com>
2024-07-12 22:24:51 +0200

    small mobilenet

--- scripts/networks/nn_2d.py
+++ scripts/networks/nn_2d.py
@@ -5,14 +5,14 @@ import torchvision.models as models
 class MobileNetV3_Segmentation(nn.Module):
     def __init__(self, num_classes):
         super(MobileNetV3_Segmentation, self).__init__()
-        self.backbone = models.mobilenet_v3_large(pretrained=True).features
+        self.backbone = models.mobilenet_v3_small(pretrained=True).features
         
         self.segmentation_head = nn.Sequential(
-            nn.Conv2d(960, 512, kernel_size=3, padding=1),  # MobileNetV3 large has 960 output channels
+            nn.Conv2d(576, 256, kernel_size=3, padding=1),  # MobileNetV3 large has 960 output channels
             nn.ReLU(inplace=True),
-            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, num_classes, kernel_size=1),  # Output num_classes channels
+            nn.Conv2d(128, num_classes, kernel_size=1),  # Output num_classes channels
             nn.Upsample(scale_factor=32, mode='bilinear', align_corners=False)  # Upsample to the original image size
         )
         
